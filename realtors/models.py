58cbd89e6348def97390cafca43ffe1eff964c5a
TRUNG NGUYEN <nhht77@gmail.com>
2019-07-22 23:38:19 +0300

    Realtor Model & Run Migrate

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Realtor(models.Model):
+    name         = models.CharField(max_length=200)
+    email        = models.EmailField(max_length=254)
+    description  = models.TextField(blank=True)
+    phone        = models.CharField(max_length=20)  
+    photo        = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_mvp       = models.BooleanField(default=False)
+    hire_date    = models.DateField(auto_now=True, blank=True)  
+    def __str__(self):
+        return self.name
