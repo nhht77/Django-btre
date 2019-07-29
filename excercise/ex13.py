4bdc0fd5eb9ff909f3dbb916d72bc300c8be829b
TRUNG NGUYEN <nhht77@gmail.com>
2019-07-29 23:56:09 +0300

    Ex 13 - Parameter, Unpacking & Variables

---
+++ excercise/ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+scripts, first, second =  argv
+
+something = input()
+
+print(f'This script is called {scripts}')
+print(f'This first is called {first}')
+print(f'This 2nd is called {second}')
+print(f'This something is called {something}')
