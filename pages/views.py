10507052414b80708e45220c1e504640fb14317e
TRUNG NGUYEN <nhht77@gmail.com>
2019-08-05 23:28:47 +0300

    About Page Dynamic Content

--- pages/views.py
+++ pages/views.py
@@ -12,4 +12,10 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+
+    realtors     = Realtor.objects.order_by('-hire_date') 
+
+    # Get MVP Realtor
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context      = { 'realtors': realtors, 'mvp_realtors': mvp_realtors}
+    return render(request, 'pages/about.html', context)
