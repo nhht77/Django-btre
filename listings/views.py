49a6d366414fddd09951bf2328686f2b70981113
TRUNG NGUYEN <nhht77@gmail.com>
2019-08-05 22:43:09 +0300

    Pagination, Order & Filter - Combine Django doc & Bootstrap doc

--- listings/views.py
+++ listings/views.py
@@ -6,13 +6,13 @@ from .models import Listing
 # Create your views here.
 def index(request):
 
-    listings  = Listing.objects.all()
+    listings  = Listing.objects.order_by('-list_date')
     paginator = Paginator(listings, 3)
 
     page      = request.GET.get('page')
-    paged_listing = paginator.get_page(page) 
+    paged_listings = paginator.get_page(page) 
 
-    context = {'listings': paged_listing}
+    context = {'listings': paged_listings}
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
