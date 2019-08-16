58960cdd5f558c5a3955744a49dc732412e50bdb
TRUNG NGUYEN <nhht77@gmail.com>
2019-08-17 01:35:47 +0300

    Search Form Choice

--- pages/views.py
+++ pages/views.py
@@ -3,11 +3,17 @@ from django.http import HttpResponse
 
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import state_choices, price_choices, bedroom_choices
 
 def index(request):
 
     listings  = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    context   = { 'listings': listings}
+    context = {
+        'listings': listings,
+        'bedroom_choices':bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices
+    }
 
     return render(request, 'pages/index.html', context)
 
